f1823ba340ea35cf496d9c9695f63601ffff00ab
vstyoma <testiiing44679@gmail.com>
2022-11-07 19:40:47 +0300

    Update bot.py
    created a second part//

--- bot.py
+++ bot.py
@@ -1,7 +1,24 @@
 import logging
+
+import random
+
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
 import config
+
+#****************PASSWORD GENERATOR*******************
+pas = ""
+for i in range(10):
+    pas = pas + random.choice(list('1234567890'))
+pas1 = ""
+for i in range(10):
+    pas1 = pas1 + random.choice(list('abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+pas2 = ""
+for i in range(10):
+    pas2 = pas2 + random.choice(list('1234567890abcdefghigklmnopqrstuvyxwzABCDEFGHIGKLMNOPQRSTUVYXWZ'))
+
+#****************PASSWORD GENERATOR*******************
 
 TOKEN = (config.token)
 
@@ -14,29 +31,46 @@ dp = Dispatcher(bot)
 async def welcome(message):
     await message.reply("Hey!\nLet's start creating passwords!\nUse the /password command to create password.")
 
-buttons = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text="Numbers", callback_data="numbers"),
-                                                InlineKeyboardButton(text="Letters", callback_data="letters"),
-                                                InlineKeyboardButton(text="Numbers and Letters", callback_data="numbersandletters"))
+buttons = InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(text="Numbers🔢", callback_data="numbers"),
+                                                InlineKeyboardButton(text="Letters🔠", callback_data="letters"),
+                                                InlineKeyboardButton(text="Numbers and Letters🔡🔢", callback_data="numbersandletters"))
 
 @dp.message_handler(commands=['password'])
 async def password(message):
-  
-   
-   
-   
-    
     await message.answer("Shall we use numbers and letters?", reply_markup=buttons)
 
 @dp.callback_query_handler(text="numbers")
 async def nums_callback(callback: types.CallbackQuery):
-    await callback.message.answer(text="Ok. Enter the number of characters from 1 to 16")
+    await callback.message.answer(text="Ok. Your generated password is:")
+    await callback.message.answer(pas)
+
 @dp.callback_query_handler(text="letters")
 async def nums_callback(callback: types.CallbackQuery):
-    await callback.message.answer(text="Ok. Enter the number of characters from 1 to 16")
+    await callback.message.answer(text="Ok. Your generated password is:")
+    await callback.message.answer(pas1)
+
 @dp.callback_query_handler(text="numbersandletters")
 async def nums_callback(callback: types.CallbackQuery):
-    await callback.message.answer(text="Ok. Enter the number of characters from 1 to 16")   
+    await callback.message.answer(text="Ok. Your generated password is:")   
+    await callback.message.answer(pas2)
 
+#***********DO NOT USE************
+"""@dp.message_handler()
+async def final_password_nums(message: types.Message):
+    await message.answer("Your generated password is: ")
+    await message.answer(pas)
+
+dp.message_handler()
+async def final_password_letters(message: types.Message):
+    await message.answer("Your generated password is: ")
+    await message.answer(pas1)
+    
+
+dp.message_handler()
+async def final_password_numsandletters(message: types.Message):
+    await message.answer("Your generated password is: ")
+    await message.answer(pas2)"""
+#***********DO NOT USE************    
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
